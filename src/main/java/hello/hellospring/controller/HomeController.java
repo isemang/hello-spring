bbe659759c6062a8c360e95390e5940af22bfbe5
isemang <a55306039@gmail.com>
2021-06-08 17:20:32 +0900

    [2021.06.08] 박민진-회원 추가/회원 목록 확인 메서드 추가

---
+++ src/main/java/hello/hellospring/controller/HomeController.java
@@ -0,0 +1,17 @@
+package hello.hellospring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "home";
+    }
+}
+
